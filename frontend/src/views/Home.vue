980df058cf474734523b01fa0d80414f4e33e1d1
Felipe Fernandes <mewbi.fernandes@gmail.com>
2024-05-17 11:38:06 -0300

    Improve responsive page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,82 +1,94 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full h-screen bg-white dark:bg-gray-800 p-8">
-    <div class="flex flex-col items-center justify-center h-full">
-      <img src="../assets/go-fight.png" class="w-40 h-40 mb-6" />
+  <div
+    class="flex flex-col justify-between w-full h-full min-h-screen bg-white dark:bg-gray-800 p-8"
+  >
+    <div></div>
+    
+    <div class="flex flex-col md:flex-row md:gap-x-2 items-center justify-center h-full">
+      <div class="w-full max-w-lg flex flex-col items-center">
+        <img src="../assets/go-fight.png" class="w-40 h-40 mb-6" />
 
-      <h1 class="text-2xl font-semibold text-gray-900 dark:text-white mb-6">Furi</h1>
-      <div class="dark:text-white w-full max-w-md text-wrap space-y-2 pl-1 pr-1">
-        <p>A <b>F</b>ast way to short an <b>URI</b>, in other words, an URL shortener.</p>
-        <p>It's simple, just paste your URL and get a small one.</p>
-        <p><s>And more, you can check analytics metrics of your URL.</s></p>
-      </div>
+        <h1 class="text-2xl font-semibold text-gray-900 dark:text-white mb-6">Furi</h1>
 
-      <div class="mt-12 w-full max-w-md relative mb-4">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 text-gray-800 dark:text-gray-400"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
-            />
-          </svg>
-        </div>
-        <input
-          type="text"
-          id="input-group-1"
-          class="bg-gray-100 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="https://github.com/Mewbi"
-        />
-        <button
-          type="submit"
-          class="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white dark:text-black bg-gray-600 rounded-e-md border hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-200 dark:hover:bg-gray-300 dark:focus:ring-gray-800"
-        >
-          Short
-        </button>
-      </div>
-
-      <div class="w-full max-w-md mt-8">
-        <h2 class="text-lg text-center font-bold text-gray-900 dark:text-white mb-4">
-          Your Shortened URLs
-        </h2>
-        <div class="space-y-4">
-          <div
-            class="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-3 rounded-md"
-          >
-            <div>
-              <p class="text-sm text-gray-900 dark:text-white mb-1">short.ly/3rFg4</p>
-              <p class="text-xs text-gray-500">https://github.com/Mewbi</p>
-            </div>
-            <button
-              class="dark:text-white inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3"
-            >
-              Copy
-            </button>
-          </div>
-          <div
-            class="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-3 rounded-md"
-          >
-            <div>
-              <p class="text-sm text-gray-900 dark:text-white mb-1">short.ly/7tYh1</p>
-              <p class="text-xs text-gray-500">https://github.com/Mewbi</p>
-            </div>
-            <button
-              class="dark:text-white inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3"
-            >
-              Copy
-            </button>
-          </div>
+        <div class="dark:text-white w-full max-w-md text-wrap space-y-2 pl-1 pr-1">
+          <p>A <b>F</b>ast way to short an <b>URI</b>, in other words, an URL shortener.</p>
+          <p>It's simple, just paste your URL and get a small one.</p>
+          <p><s>And more, you can check analytics metrics of your URL.</s></p>
         </div>
       </div>
 
+      <div class="w-full max-w-lg flex flex-col items-center">
+        <div class="mt-12 w-full max-w-md relative mb-4">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-2.5 pointer-events-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 text-gray-800 dark:text-gray-400"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
+              />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="input-group-1"
+            class="bg-gray-100 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="https://github.com/Mewbi"
+          />
+          <button
+            type="submit"
+            class="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white dark:text-black bg-gray-600 rounded-e-md border hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-200 dark:hover:bg-gray-300 dark:focus:ring-gray-800"
+          >
+            Short
+          </button>
+        </div>
+
+        <div class="w-full max-w-md mt-8 mb-2">
+          <h2 class="text-lg text-center font-bold text-gray-900 dark:text-white mb-4">
+            Your Shortened URLs
+          </h2>
+          <div class="space-y-4">
+            <div
+              class="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-3 rounded-md"
+            >
+              <div>
+                <p class="text-sm text-gray-900 dark:text-white mb-1">short.ly/3rFg4</p>
+                <p class="text-xs text-gray-500">https://github.com/Mewbi</p>
+              </div>
+              <button
+                class="dark:text-white inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3"
+              >
+                Copy
+              </button>
+            </div>
+            <div
+              class="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-3 rounded-md"
+            >
+              <div>
+                <p class="text-sm text-gray-900 dark:text-white mb-1">short.ly/7tYh1</p>
+                <p class="text-xs text-gray-500">https://github.com/Mewbi</p>
+              </div>
+              <button
+                class="dark:text-white inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3"
+              >
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer section -->
+    <div class="flex flex-col items-center text-center">
       <div class="flex justify-center w-full max-w-md mt-8 space-x-5">
         <a
           target="_blank"
@@ -84,7 +96,7 @@
           class="hover:underline hover:text-gray-900 me-4 md:me-6 flex gap-2"
         >
           <svg
-            class="w-6 h-6 text-gray-600 dark:text-gray-400"
+            class="w-5 h-5 text-gray-600 dark:text-gray-400"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -109,7 +121,7 @@
             fill="none"
             role="img"
             aria-labelledby="akli8arz4m5qzg9bckn62cdne70zuumh"
-            class="w-6 h-6 octicon text-gray-600 dark:text-gray-400"
+            class="w-5 h-5 octicon text-gray-600 dark:text-gray-400"
           >
             <title>LinkedIn</title>
             <g clip-path="url(#clip0_202_91845)">
@@ -121,7 +133,16 @@
           </svg>
         </a>
       </div>
+
+      <div class="w-full max-w-md">
+        <hr class="my-2 border-gray-400 sm:mx-auto lg:my-2" />
+        <span class="block text-sm text-gray-500 sm:text-center">
+          © {{ new Date().getFullYear() }}
+          <RouterLink :to="{ name: 'home' }" class="hover:underline"> - Furi</RouterLink>
+        </span>
+      </div>
     </div>
+
   </div>
 </template>
 
